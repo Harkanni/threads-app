36f08366937c6f5cbd4408298c7fcb3c1d93fc7e
harkanni <harkanzyempire001@gmail.com>
2023-08-22 18:08:30 +0100

    onboarding done

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -16,8 +16,12 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { UserValidation } from "@/lib/validations/user";
 import { z } from "zod";
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from '@/lib/uploadthing';
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
    user: {
@@ -31,25 +35,73 @@ interface Props {
    btnTitle: string;
 }
 
-const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
-   e.preventDefault();
-}
-
-function onSubmit(values: z.infer<typeof UserValidation>) {
-   // Do something with the form values.
-   // ✅ This will be type-safe and validated.
-   console.log(values)
-}
 
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
+   const [ files, setFiles ] = useState<File[]>([])
+   const { startUpload } = useUploadThing("media")
+   const router = useRouter()
+   const pathName = usePathname()
+
+   const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
+      e.preventDefault();
+
+      const fileReader = new FileReader();
+      if(e.target.files && e.target.files.length > 0) {
+         const file = e.target.files[0];
+
+         setFiles(Array.from(e.target.files));
+
+         if(!file.type.includes("image")) return;
+
+         fileReader.onload = async (event) => {
+            const imageDataUrl = event.target?.result?.toString() || '';
+
+            fieldChange(imageDataUrl)
+         }
+
+         fileReader.readAsDataURL(file)
+      }
+   }
+
+   const onSubmit = async function onSubmit(values: z.infer<typeof UserValidation>) {
+      const blob = values.profile_photo
+
+      const hasImageChanged = isBase64Image(blob);
+
+      if(hasImageChanged) {
+         const imgRes = await startUpload(files)
+
+         if(imgRes && imgRes[0].fileUrl) {
+            values.profile_photo = imgRes[0].fileUrl
+         }
+      }
+
+      
+      await updateUser({
+         userId: user.id,
+         bio: values.bio,
+         name: values.name,
+         username: values.username,
+         image: values.profile_photo,
+         path: pathName
+      });
+
+      if(pathName === '/profile/edit') {
+         router.back();
+      } else {
+         router.push('/');
+      }
+   }
+
+
    const form = useForm({
       resolver: zodResolver(UserValidation),
       defaultValues: {
-         profile_photo: '',
-         name: '',
-         username: '',
-         bio: '',
+         profile_photo: user?.image || '',
+         name: user?.name || '',
+         username: user?.username || '',
+         bio: user?.bio || '',
       }
    })
    return (
@@ -97,11 +149,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                control={form.control}
                name="name"
                render={({ field }) => (
-                  <FormItem className="flex items-center gap-3 w-full">
+                  <FormItem className="flex flex-col gap-3 w-full">
                      <FormLabel className="text-base-semibold text-light-2">
                         Name
                      </FormLabel>
-                     <FormControl className="flex-1 text-base-semibold text-gray-200">
+                     <FormControl>
                         <Input
                            type="text"
                            className="account-form_input no-focus"
@@ -116,11 +168,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                control={form.control}
                name="username"
                render={({ field }) => (
-                  <FormItem className="flex items-center gap-3 w-full">
+                  <FormItem className="flex flex-col gap-3 w-full">
                      <FormLabel className="text-base-semibold text-light-2">
                         Username
                      </FormLabel>
-                     <FormControl className="flex-1 text-base-semibold text-gray-200">
+                     <FormControl>
                         <Input
                            type="text"
                            className="account-form_input no-focus"
@@ -134,11 +186,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                control={form.control}
                name="bio"
                render={({ field }) => (
-                  <FormItem className="flex items-center gap-3 w-full">
+                  <FormItem className="flex flex-col gap-3 w-full">
                      <FormLabel className="text-base-semibold text-light-2">
                         Bio
                      </FormLabel>
-                     <FormControl className="flex-1 text-base-semibold text-gray-200">
+                     <FormControl>
                         <Textarea
                            rows={10}
                            className="account-form_input no-focus"
